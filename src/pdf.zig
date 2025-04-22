8ee4f3d60b0b11276b724a1180aafac069adf31b
bbilandzia <bbilandzia@csu.fullerton.edu>
2025-04-22 11:58:34 -0700

    ok so upside down pdf and it redraws every frame, so probably need to fix that
    so its upside down, and redraws every frame, so probably need to fix
    that, also only prints first page, so probably like a loop or something
    needs to be in place or just read all of the pages at the same time????

--- src/pdf.zig
+++ src/pdf.zig
@@ -1,63 +1,46 @@
 const c = @cImport(@cInclude("mupdf/fitz.h"));
 const std = @import("std");
-const errors = @import("errors.zig");
-
 pub const PdfImage = struct {
     data: [*]u8,
     width: c_int,
     height: c_int,
 };
 
-pub fn init(file: [:0]const u8) !PdfImage {
-    const ctx = c.fz_new_context(null, null, c.FZ_STORE_UNLIMITED) orelse {
-        std.debug.print("Filed to create mupdf conext\n", .{});
-        return errors.DocumentError.FailedToCreateContext;
-    };
-    errdefer c.fz_drop_context(ctx);
+pub fn image(file: [:0]const u8) !PdfImage {
+    const c_file: [*c]const u8 = file.ptr;
 
+    const ctx = c.fz_new_context(null, null, c.FZ_STORE_UNLIMITED);
     c.fz_register_document_handlers(ctx);
+    std.debug.print("New Context Done\n", .{});
     const page_num: u16 = 0;
-
     //other test
-    const doc = c.fz_open_document(ctx, file.ptr) orelse {
-        std.debug.print("Failed to open document: {s}\n", .{c.fz_caught_message(ctx)});
-        return errors.DocumentError.FailedToOpenDocument;
-    };
-    errdefer c.fz_drop_document(ctx, doc);
-    std.debug.print("File Path: {s}\n", .{file});
-
-    //const page_count = @as(u16, @intCast(c.fz_count_pages(ctx, doc)));
+    const doc = c.fz_open_document(ctx, c_file.?);
+    std.debug.print("File Path: {c}\n", .{file});
+    //const doc = c.fz_open_document(ctx, file.ptr);
+    std.debug.print("Document Opened\n", .{});
+    //const page = c.fz_load_page(ctx, doc, 0);
     const page = c.fz_load_page(ctx, doc, page_num);
-    defer c.fz_drop_page(ctx, page);
+    std.debug.print("Page Loaded\n", .{});
+    // testing
+    const scale: f32 = 1.0;
+    const ctm = c.fz_scale(scale, scale);
 
-    //const bound = c.fz_bound_page(ctx, page);
-
-    const view_width: f32 = 500;
-    const view_height: f32 = 500;
-
-    const bbox = c.fz_make_irect(
-        0,
-        0,
-        @intFromFloat(view_width),
-        @intFromFloat(view_height),
-    );
-
-    //const scale: f32 = 1.0;
-
+    //const ctm: c.fz_matrix = undefined;
     std.debug.print("ctm created\n", .{});
-    const pix = c.fz_new_pixmap_with_bbox(ctx, c.fz_device_rgb(ctx), bbox, null, 1);
+    //std.debug.print("ctm: {c}\n", .{ctm});
+    const pix = c.fz_new_pixmap_from_page(ctx, page, ctm, c.fz_device_rgb(ctx), 1);
 
-    //const ctm = c.fz_scale(scale, scale);
-
-    const data = c.fz_pixmap_samples(ctx, pix);
+    // figure out what these are
+    //const data = c.fz_pixmap_samples(ctx, pix);
     const width = c.fz_pixmap_width(ctx, pix);
     const height = c.fz_pixmap_height(ctx, pix);
     std.debug.print("Width: {d}\n", .{width});
     std.debug.print("Height: {d}\n", .{height});
+    //std.debug.print("Data: {d}\n", .{data});
 
     return PdfImage{
-        .data = data,
-        .width = width,
-        .height = height,
+        .data = c.fz_pixmap_samples(ctx, pix),
+        .width = c.fz_pixmap_width(ctx, pix),
+        .height = c.fz_pixmap_height(ctx, pix),
     };
 }
